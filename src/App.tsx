bcbb73bf496ed6b55bcf9ebcb458d09471faf98a
Shiroganari <shiroganari@gmail.com>
2023-02-18 00:17:27 +0700

    feat: create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import Header from '@/layouts/Header';
+import Home from "@/pages/Home";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <Header/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
   )
 }
 
