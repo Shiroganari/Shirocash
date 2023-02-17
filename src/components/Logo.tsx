d1667c1341093243b67b5739c0f2ec467e4fe74a
Shiroganari <shiroganari@gmail.com>
2023-02-17 23:31:03 +0700

    feat: create 'Logo' component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/components/logo.module.scss';
+
+function Header() {
+  return (
+    <a className={styles.logo} href="#">Shirocash</a>
+  )
+}
+
+export default Header;
